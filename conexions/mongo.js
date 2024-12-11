d9b2ed8b6b81820c16abcdf43fdaf9106813babc
gabriiel23 <gabriel.diaz.23.02.05@gmail.com>
2024-12-11 16:22:50 -0500

    Cambio de la conexion a mongo

--- conexions/mongo.js
+++ conexions/mongo.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
-require('dotenv').config(); 
+require('dotenv').config();
 
-const dbURI = process.env.MONGODB_URI; 
+const dbURI = process.env.MONGODB_URI;
 
-mongoose.connect(dbURI);
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,  // Tiempo de espera para intentar la conexión
+  socketTimeoutMS: 45000,          // Tiempo de espera para inactividad
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Error de conexión a la base de datos:'));
